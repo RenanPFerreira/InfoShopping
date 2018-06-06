9d904a0236feddf7db4ea7b2eafc33895736b08b
Gabriel Mathias <gmathias1717@gmail.com>
2018-06-06 09:37:17 -0300

    Adicionado Classes

---
+++ app/src/main/java/com/example/opet/infoshopping/Activity/ShoppingsActivity.java
@@ -0,0 +1,22 @@
+package com.example.opet.infoshopping.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.opet.infoshopping.R;
+
+/**
+ * Created by opet on 06/06/2018.
+ */
+
+public class ShoppingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoppings);
+    }
+
+    
+
+}
